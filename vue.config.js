fd99a50a1ce76a240fe94a068f1bf1a7da9e6fc1
kobayashikazu <zeronight0905@gmail.com>
2020-12-13 12:05:38 +0900

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  devServer: {
-    proxy: {
-      "/services/": {
-        target:  process.env.USE_LOCAL_SERVER ? 'https://stoic-kare-a632f2.netlify.app' : 'http://svcs.ebay.com',
-      }
+    devServer: {
+      proxy: "http://svcs.ebay.com",
     }
-  }
-};
+  };
